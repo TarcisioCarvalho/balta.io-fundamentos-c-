3907f6bf9d5cc0273e79fad2945559855188b0db
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-08-07 22:32:12 -0300

    .NET fundamentals

---
+++ Teste/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+const int IDADE_MININA=2;
+string text = "T@";
+Console.WriteLine(text);
---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,7 @@
+namespace Teste
+{
+    public class Program
+    {
+        
+    }
+}
